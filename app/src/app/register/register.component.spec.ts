e28cd9dc0176a3762b4aa3508b936213b4934887
Elodie OLLIVIER (A1) <f41374-a1@distributionelectricitetest.onmicrosoft.com>
2024-01-02 15:35:14 +0100

    APP: test recup html pour spec.ts

--- app/src/app/register/register.component.spec.ts
+++ app/src/app/register/register.component.spec.ts
@@ -21,4 +21,35 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('form should be invalid', () => {
+    expect(component.form.valid).toBeFalsy();
+});
+
+it('should submit the form when the submit button is clicked', () => {
+  const compiled = fixture.debugElement.nativeElement;
+  const btnEl = compiled.querySelector('#submit-btn');
+  const fnc = spyOn(component, 'onSubmit');
+
+  btnEl.click();
+  fixture.detectChanges();
+
+  expect(fnc).toHaveBeenCalled();
+})
+
+it('should display alert message when onSubmit is called but form is invalid', () => {
+  const compiled = fixture.debugElement.nativeElement;  
+  const btnEl = compiled.querySelector('#submit-btn');  
+  expect(compiled.querySelector('#errorEmailEmpty')).toEqual(null);
+  btnEl.click();
+  component.onSubmit();
+  fixture.detectChanges();
+  console.log("form email: ",component.form.email);
+  const afterClickCompiled = fixture.debugElement.nativeElement;   
+  expect(afterClickCompiled.querySelector('#errorEmailEmpty')).not.toEqual(null);  
+ // expect(afterClickCompiled.querySelector('#errorEmailEmpty').innerText).toEqual('Email is required');
+  //expect(afterClickCompiled.querySelector('#errorPasswordEmpty').innerText).toEqual('Password is required');
+ 
+})
+
 });
